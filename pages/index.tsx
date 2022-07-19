06880774a8f01e7a24514000eec2b90aca1cdd72
Malonic <97789166+Malonic@users.noreply.github.com>
2022-07-20 00:02:29 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,8 @@ const Home: NextPage = () => {
             </p>
             <div className="flex px-16 w-3/4">
               <div className="text-xl mt-4 sm:mt-0 font-medium rounded-xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-blue-500 hover:to-green-50 text-white px-6 py-4 mr-4">
-                <Link href="/events">Digex 2022 </Link>
+                <Link href="/events">Hi Res Topic 
+ </Link>
               </div>
               <div className="text-xl mt-4 sm:mt-0 font-medium rounded-xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-blue-500 hover:to-green-50 text-white px-6 py-4">
                 <a href="https://drive.google.com/file/d/1MOno8ZT91EjpLeuwzecmVHhG0CNlRvAN/view?usp=sharing">
